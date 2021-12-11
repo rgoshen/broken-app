d69ef998fc8c341d3603013df0f957d243a28cd0
rgoshen <rick.goshen@gmail.com>
2021-12-11 14:58:54 -0700

    reset app.js

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
 const express = require('express');
-// let axios = require( 'axios' );
-
+let axios = require('axios');
 var app = express();
 
-// app.post('/', function (req, res, next) {
-//   try {
-//     let results = req.body.developers.map(async (d) => {
-//       return await axios.get(`https://api.github.com/users/${d}`);
-//     });
-//     let out = results.map((r) => ({ name: r.data.name, bio: r.data.bio }));
+app.post('/', function(req, res, next) {
+  try {
+    let results = req.body.developers.map(async d => {
+      return await axios.get(`https://api.github.com/users/${d}`);
+    });
+    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
 
-//     return res.send(JSON.stringify(out));
-//   } catch (err) {
-//     console.log(err);
-//     next(err);
-//   }
-// });
-
-// middleware to parse req.body
-app.use(express.json);
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/:name', (req, res, next) => {
-  return res.send(`Welcome to node express ${req.params.name}`);
+    return res.send(JSON.stringify(out));
+  } catch {
+    next(err);
+  }
 });
 
-app.get('/dev', (req, res, next) => {
-  return res.send('sent');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running...');
-});
+app.listen(3000);
