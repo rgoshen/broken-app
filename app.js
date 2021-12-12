29c2b8b5095b59af6dcdd9b0b414ca6a968a0e93
rgoshen <rick.goshen@gmail.com>
2021-12-11 18:47:28 -0700

    fixed post route

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 let axios = require('axios');
+const morgan = require('morgan');
+
 var app = express();
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
+app.post('/', async function (req, res, next) {
+  const names = req.body.developers;
+  try {
+    let results = await axios.all(
+      names.map((name) => axios.get(`https://api.github.com/users/${name}`))
+    );
+    let out = results.map((r) => {
+      return { name: r.data.name, bio: r.data.bio };
+    });
+
+    return res.json(out);
+  } catch (err) {
     next(err);
   }
 });
 
-app.listen(3000);
+app.listen(3000, (error) => {
+  if (!error) {
+    console.log(`Serving is now running on http://localhost:3000...`);
+  } else {
+    console.log("Error occurred, server can't start", error);
+  }
+});
